89a1dfdbde56e3351179289a2c82cfbacd8d19f3
Rahul Somasunderam <rahul.som@gmail.com>
2017-03-19 16:48:54 -0700

    Initial commit

---
+++ src/main/groovy/com/github/rahulsom/swci/SwciApplication.groovy
@@ -0,0 +1,8 @@
+package com.github.rahulsom.swci
+
+class SwciApplication {
+    static void main(String[] args) {
+        def serverKey = System.getenv('FIREBASE_SWCI_SERVER_KEY')
+        // new CheckinSupport().checkin('12345', 'John', 'Smith')
+    }
+}
